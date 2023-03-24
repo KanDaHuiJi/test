2637c21f7feff6e17c106a5a432f7ba9af649d72
qiao <qiao@qq.com>
2023-03-24 17:41:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author Q
+ * @date 2023/3/24-17:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
